c37241007b55fa8f400c73def48fe17bc2f19dde
kathan24 <kathan24@gmail.com>
2017-08-13 15:46:04 -0700

    Permutations of a String

--- src/permutationOfAString.java
+++ src/permutationOfAString.java
@@ -8,12 +8,10 @@ public class permutationOfAString {
 
     public static void printAllPermutation(String result, String remainingString){
         if(remainingString.length() > 1){
-            char[] remainingCharArray = remainingString.toCharArray();
-
-            for(int i = 0; i < remainingCharArray.length; i++){
+            for(int i = 0; i < remainingString.length(); i++){
                 StringBuilder tempBuilder = new StringBuilder(remainingString);
                 tempBuilder.deleteCharAt(i);
-                printAllPermutation(result + remainingCharArray[i], tempBuilder.toString());
+                printAllPermutation(result + remainingString.charAt(i), tempBuilder.toString());
             }
         }else{
             System.out.println(result + remainingString);
